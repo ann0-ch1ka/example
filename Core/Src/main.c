4f5e1435c26478959cd7c9485d729159b05e2468
anno_chika <1309661362@qq.com>
2024-12-02 17:32:19 +0800

    xiugai

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -28,7 +28,7 @@
 #include "OLED.h"
 #include "stdio.h"
 #include "BH1750.h"
-// hello i'm tomas
+// hello 
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
