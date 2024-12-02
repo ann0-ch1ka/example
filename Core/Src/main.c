01dbc2933037d84cd25f97b14abdc970f349ac50
anno_chika <1309661362@qq.com>
2024-12-02 17:07:00 +0800

    first commit

---
+++ Core/Src/main.c
@@ -0,0 +1,353 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2024 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "i2c.h"
+#include "tim.h"
+#include "usart.h"
+#include "gpio.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "OLED.h"
+#include "stdio.h"
+#include "BH1750.h"
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+#define DHT11_HIGH     HAL_GPIO_WritePin(GPIOA, DHT11_Pin,	GPIO_PIN_SET)
+#define DHT11_LOW      HAL_GPIO_WritePin(GPIOA, DHT11_Pin, GPIO_PIN_RESET)
+ 
+#define DHT11_IO_IN      HAL_GPIO_ReadPin(GPIOA, DHT11_Pin)
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+/* USER CODE BEGIN PFP */
+void Delay_us(uint16_t delay);              
+void DHT11_OUT(void);													
+void DHT11_IN(void);                          
+void DHT11_Strat(void);				                
+uint8_t DHT11_Check(void);                   
+uint8_t DHT11_Read_Bit(void);                 
+uint8_t DHT11_Read_Byte(void);                
+uint8_t DHT11_Read_Data(uint8_t* temp , uint8_t* humi); 
+uint8_t num_len(uint32_t num){
+    if(num==0)return 1;
+    uint8_t i=0;
+    while(num>0){
+        num/=10;
+        i++;
+    }
+    return i;
+}
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+    uint8_t temperature = 1 ; 
+	uint8_t humidity = 1;
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_USART1_UART_Init();
+  MX_TIM3_Init();
+  MX_I2C1_Init();
+  /* USER CODE BEGIN 2 */
+  DHT11_Strat();
+  OLED_Init();
+  Init_BH1750();
+  OLED_ShowCHinese(1,1,0);
+  OLED_ShowCHinese(1,2,2);
+  OLED_ShowCHinese(1,3,3);
+  OLED_ShowCHinese(2,1,1);
+  OLED_ShowCHinese(2,2,2);
+  OLED_ShowCHinese(2,3,3);
+  OLED_ShowCHinese(1,5,4);
+  OLED_ShowChar(2,9,'%');
+  OLED_ShowCHinese(3,1,5);
+  OLED_ShowCHinese(3,2,6);
+  OLED_ShowCHinese(3,3,3);
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+      DHT11_Read_Data(&temperature,&humidity);
+      Init_BH1750();
+      OLED_ShowNum(1,6,(uint32_t)temperature,num_len(temperature));
+      OLED_ShowNum(2,6,(uint32_t)humidity,num_len(humidity));
+      HAL_Delay(500);
+      uint32_t light = Value_GY30();
+      OLED_ShowNum(3,6,light,num_len(light));
+
+      
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/* USER CODE BEGIN 4 */
+void Delay_us(uint16_t delay)
+{
+	__HAL_TIM_DISABLE(&htim3);
+	__HAL_TIM_SET_COUNTER(&htim3,0);
+	__HAL_TIM_ENABLE(&htim3);
+	uint16_t curCnt=0;
+	while(1)
+	{
+		curCnt=__HAL_TIM_GET_COUNTER(&htim3);
+		if(curCnt>=delay)
+			break;
+	}
+	__HAL_TIM_DISABLE(&htim3);
+}
+ 
+void DHT11_OUT(void)
+{
+	GPIO_InitTypeDef  GPIO_InitStruct = {0};
+ 
+	GPIO_InitStruct.Pin = GPIO_PIN_8;
+	GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+	GPIO_InitStruct.Pull = GPIO_PULLUP;
+	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+	HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+}
+ 
+void DHT11_IN(void)
+{
+	GPIO_InitTypeDef  GPIO_InitStruct = {0};
+ 
+	GPIO_InitStruct.Pin  = GPIO_PIN_8;
+	GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+	GPIO_InitStruct.Pull = GPIO_NOPULL;
+	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+	HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+}
+ 
+ 
+void DHT11_Strat(void)
+{
+	DHT11_OUT();
+	DHT11_LOW;
+	HAL_Delay(20);
+	DHT11_HIGH;
+	Delay_us(30);
+}
+ 
+ 
+uint8_t DHT11_Check(void)
+{
+	uint8_t retry = 0 ;
+	DHT11_IN();
+		
+	while(DHT11_IO_IN && retry <100) 
+	{
+		retry++;
+		Delay_us(1);//1us
+	}
+	if(retry>=100) 
+	{return  1;}
+	 else retry =  0 ;
+		
+	while(!DHT11_IO_IN && retry<100)
+	{
+		retry++;
+		Delay_us(1);//1us
+	}
+		
+	if(retry>=100)
+	{return 1;}
+	return 0 ;
+}
+ 
+ 
+uint8_t DHT11_Read_Bit(void)
+{
+	uint8_t retry = 0 ;
+	while(DHT11_IO_IN && retry <100)
+	{
+		retry++;
+		Delay_us(1);
+	}
+		retry = 0 ;
+	while(!DHT11_IO_IN && retry<100)
+	{
+		retry++;
+		Delay_us(1);
+	}
+	Delay_us(40);
+	if(DHT11_IO_IN) return 1;
+	else 
+	return 0 ;
+}
+ 
+ 
+uint8_t DHT11_Read_Byte(void)
+{
+	uint8_t i , dat ;
+	dat = 0 ;
+	for(i=0; i<8; i++)
+	{
+		dat <<= 1;
+		dat |= DHT11_Read_Bit();
+	}
+	return dat ; 
+}
+ 
+ 
+uint8_t DHT11_Read_Data(uint8_t* temp , uint8_t* humi)
+{
+	uint8_t buf[5];
+	uint8_t i;
+	DHT11_Strat();
+    if(DHT11_Check() == 0)
+	{
+		for(i=0; i<5; i++)
+		{
+			buf[i] = DHT11_Read_Byte();
+		}
+		if(buf[0]+buf[1]+buf[2]+buf[3] == buf[4])
+		{
+			*humi = buf[0];
+			*temp = buf[2];
+		}
+	}else return 1;
+    return 0;
+}
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
